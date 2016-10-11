37104da0d2c39878e042acf490ebf933baa60e6a
sanvalask <sanvala.shekar@gmail.com>
2016-10-10 22:48:43 -0400

    new class

---
+++ myfirstjpa/src/main/java/org/jpa/emp/CreateEmployee.java
@@ -0,0 +1,28 @@
+package org.jpa.emp;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class CreateEmployee {
+
+	   public static void main( String[ ] args ) {
+	   
+	      EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("Demo");
+	      
+	      EntityManager entitymanager = emfactory.createEntityManager( );
+	      entitymanager.getTransaction( ).begin( );
+
+	      Employ employee = new Employ( ); 
+	      employee.setEid( 1201 );
+	      employee.setEname( "Gopal" );
+	      employee.setSalary( 40000 );
+	      employee.setDeg( "Technical Manager" );
+	      
+	      entitymanager.persist( employee );
+	      entitymanager.getTransaction( ).commit( );
+
+	      entitymanager.close( );
+	      emfactory.close( );
+	   }
+	}
